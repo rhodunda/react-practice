84861a21dc6fc93be1c882dad9c16f51fa81f74d
Bryan Rhodunda <rhodunda@udel.edu>
2021-02-12 16:53:36 -0500

    created basic setup

--- src/componets/info/roster/roster.js
+++ src/componets/info/roster/roster.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import styles from './roster.module.css'
+import { render } from 'react-dom'
+import axios from 'axios'
 
 const roster = () => {
+
+    render () {
+        axios.get('https://api.nfl.com/docs/league/models/json/Team.json')
+        .then(data =>{
+            console.log(data)
+        })
+    }
     return (
     <div className={styles.layout}>roster</div>
     )
