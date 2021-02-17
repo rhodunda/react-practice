fbf9ea1b3360d12c718bf81f2474ec8fa3bca625
Bryan Rhodunda <rhodunda@udel.edu>
2021-02-16 19:05:09 -0500

    one step closer

--- src/componets/info/frontpage/mainPage.js
+++ src/componets/info/frontpage/mainPage.js
@@ -13,24 +13,39 @@ const [FrontPageData, setFrontPageData] = useState({});
 useEffect(() => {
     axios.get("http://site.api.espn.com/apis/site/v2/sports/football/nfl/news")
     .then(res =>{
-        const articles = [res.data.articles]
-        setFrontPageData(articles)
-
+        const articles = [res.data.articles];
+        setFrontPageData(articles);
+    
     })
     .catch(err => {
-        console.log(err)
-    })
+        console.log(err);
+    });
     
-    },[])
+    },[]);
 
+
+    render() {
+
+
+        
+        const newsCard = FrontPageData.map(article => {
+            <div>
+                <h3>article.headline</h3>
+                <p>article.description</p>
+                <img>article.images[0].url</img>
+                <href>article.links.web.href</href>
+            </div>
+        })
+    
 
 
     return (
         <div>
-           
+                {newsCard}
         </div>
     )
 
+    }
 
 }
 
