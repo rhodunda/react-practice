c6c900ceb80b374ba1683ed06073ba2d91262f87
Bryan Rhodunda <rhodunda@udel.edu>
2021-03-09 16:39:34 -0500

    added toollbar to team page mapped out next steps"

--- src/componets/Team.js
+++ src/componets/Team.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Toolbar from '../componets/toolbar/toolbar'
 
 
 const Team = () => {
 
+
+    // get team name from props
+    // make api call for team infomation
+    // make sure to render the write team info fix url
+
+
     return(
+        <div>
+        <Toolbar/>
         <div>Team Page</div>
+        </div>
     )
 }
 
