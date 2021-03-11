f399866eabeea6551cbd9b695a876359f40939ad
Bryan Rhodunda <rhodunda@udel.edu>
2021-03-10 22:22:15 -0500

    spit the url and got the team name

--- src/componets/Team.js
+++ src/componets/Team.js
@@ -4,7 +4,9 @@ import Toolbar from '../componets/toolbar/toolbar'
 
 const Team = () => {
 
-
+    console.log(window.location.href.replace('%20', '').split('team/')[1])
+    
+    
     // get team name from props
     // make api call for team infomation
     // make sure to render the write team info fix url
