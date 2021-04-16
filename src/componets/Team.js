d70455099777745e71b298cc8ccb998f17b47f6e
Bryan Rhodunda <rhodunda@udel.edu>
2021-04-16 10:38:54 -0400

    added team records to team page

--- src/componets/Team.js
+++ src/componets/Team.js
@@ -12,19 +12,28 @@ const Team = () => {
     const [TeamName, setTeamName] = useState()
     const [AllTeamData, setAllTeamData] = useState()
     const [CurrentTeamData, setCurrentTeamData] = useState()
+    const [CurrentTeamRecord, setCurrentTeamRecord] = useState(false)
 
     
     useEffect(() =>{
-
         setTeamName(window.location.href.replace('%20', ' ').split('team/')[1])
     })
 
     useEffect(()=> {
+
         axios.get('http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams')
         .then(res => {
-            console.log(res.data.sports[0].leagues[0].teams)
+            // console.log(res.data.sports[0].leagues[0].teams)
             const teamData = res.data.sports[0].leagues[0].teams
             setAllTeamData(teamData)
+
+            let teamName = window.location.href.replace('%20', ' ').split('team/')[1]
+            teamData.forEach(element => {
+                if(teamName === element.team.displayName) {
+                    setCurrentTeamData(element.team)
+                    setCurrentTeamRecord(element.team.record.items[0].summary)
+                }
+            });
         })
         .catch(err => console.log(err))
     }, [])
@@ -41,7 +50,10 @@ const Team = () => {
     return(
         <div>
         <Toolbar/>
-        <div className={styles.displayName}>{TeamName}</div>
+        <div className={styles.displayName}>
+            <div className={styles.teamName}>{TeamName}</div>
+            <div className={styles.teamRecond}>{CurrentTeamRecord}</div>
+        </div>
         </div>
     )
 }
